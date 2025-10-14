c7539fa1c60cd095ae15077452657da459e7e603
shyun <exriik@gmail.com>
2025-10-14 12:45:12 +0900

    251014

--- study/study/src/main/java/com/springboot/study/controller/MemberController.java
+++ study/study/src/main/java/com/springboot/study/controller/MemberController.java
@@ -1,11 +1,13 @@
 package com.springboot.study.controller;
 
+import com.springboot.study.dto.Member;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MemberController {
@@ -14,12 +16,27 @@ public class MemberController {
         return "login"; //로그인 화면, view name --> templates
     }
 
+    /**
+     * Spring Legacy 버전 (3.0 이하) --> ModelAndView 객체를 활용하여 데이터 및 view 전송
+     */
+//    @PostMapping("/login")
+//    public ModelAndView login(@RequestParam String id, @RequestParam String pass) {
+//        ModelAndView model = new ModelAndView();
+//        String result="";
+//        if (id.equals("test") && pass.equals("1234"))  result = "[ModelAndView]로그인 성공";
+//        else result = "[ModelAndView]로그인 실패";
+//
+//        model.addObject("result", result);
+//        model.setViewName("loginResult");
+//        return model;
+//    }
+
+
     @PostMapping("/login")
-    public String login(@RequestParam String id,
-                        @RequestParam String pass,
-                        Model model) {
+    public String login(Member member
+                        , Model model) {
         String result="";
-        if (id.equals("test") && pass.equals("1234")) {
+        if (member.getId().equals("test") && member.getPass().equals("1234")) {
             result = "로그인 성공";
         } else {
             result = "로그인 실패";
